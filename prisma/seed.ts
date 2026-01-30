6a4f2cda6fdc2fe00b7a1d84f1db502f1d0c323f
Ritik <ritikyadav2426@gmail.com>
2026-01-30 12:20:38 +0530

    initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+ 
+  {
+    name: "Ritik",
+    email: "ritik.io",
+    posts: {
+      create: [
+        {
+          title: "Join the Prisma Discord",
+          content: "https://pris.ly/discord",
+          published: true,
+        },
+        {
+          title: "Prisma on YouTube",
+          content: "https://pris.ly/youtube",
+        },
+      ],
+    },
+  },
+
+ 
+];
+
+export async function main() {
+
+   
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
